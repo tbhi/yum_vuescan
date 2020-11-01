190adabe7ed6753e365fc1ff3d50b59a2fc31818
Tristan Hill <tristan@me.uk.eu.org>
2020-11-01 19:52:59 +0000

    minor improvements

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ ARCH=x86_64
 VERSION=$(curl -s http://static.hamrick.com/vuescan_version.txt)
 TARGET="$REPO/$ARCH/vuescan-$VERSION-0.$ARCH.rpm"
 
-if ! [ -f "$TARGET" ]; then
+if [ "$(file -bi "$TARGET")" != 'application/x-rpm; charset=binary' ]; then
+  rm -rf "$REPO"
   mkdir -p $(dirname "$TARGET")
   curl -sLo "$TARGET" https://www.hamrick.com/files/vuex6497.rpm
   createrepo -q "$REPO"
